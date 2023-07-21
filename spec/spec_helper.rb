e91b3fcbc3776b844e18c8abb3c21e4cd0b69452
Hyeonjun Lee <hyeonjun.lee@privatenote.co.kr>
2023-07-21 13:14:51 +0900

    테스트 중 stdout, stderr에 아무것도 찍지 않도록 수정

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,4 +31,17 @@ RSpec.configure do |config|
       'be monotonically increasing'
     end
   end
+
+  original_stderr = $stderr
+  original_stdout = $stdout
+
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, 'w')
+    $stdout = File.open(File::NULL, 'w')
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 end
